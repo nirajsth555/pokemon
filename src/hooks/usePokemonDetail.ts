f63990eb7165665ba50007185f57bc0617c2ea48
niraj.shrestha <niraj.shrestha@amniltech.com>
2023-09-20 12:30:31 +0545

    modified usePokemonDetail hook

--- src/hooks/usePokemonDetail.ts
+++ src/hooks/usePokemonDetail.ts
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Pokeball from "../assets/images/pokeball.png";
 import { getPokemonDetailByName as pokemonDetail } from "../services/pokemon";
 import { IPokemon } from "../types";
 
 export function usePokemonDetail() {
     const [detail, setDetail] = useState<IPokemon>();
-
-    const [pokemonImage, setPokemonImage] = useState(Pokeball);
-    const [pokemonType, setPokemonType] = useState<any>("");
-    const [pokemonTypeList, setPokemonTypeList] = useState<any>([]);
 
     const getPokemonDetailByName = async (pokemonName: string) => {
         try {
@@ -21,11 +17,9 @@ export function usePokemonDetail() {
         }
     };
 
-    useEffect(() => {
-        setPokemonImage(detail?.sprites?.other?.["official-artwork"]?.front_shiny || Pokeball);
-        setPokemonType(detail?.types?.[0]?.type?.name);
-        setPokemonTypeList(detail?.types);
-    }, [detail])
+    const pokemonImage = detail?.sprites?.other?.["official-artwork"]?.front_shiny || Pokeball;
+    const pokemonType = detail?.types?.[0]?.type?.name;
+    const pokemonTypeList = detail?.types;
 
     return {
         getPokemonDetailByName,
